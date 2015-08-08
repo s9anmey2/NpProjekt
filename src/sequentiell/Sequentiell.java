a6cfff5cb9cbc0e27d14becd70142414db959190
Andreas <s9anmey2@stud.uni-saarland.de>
2015-08-08 18:38:40 +0200

    triviales implementiert und mit der rein sequentiellen Loesung angefangen.

--- src/sequentiell/Sequentiell.java
+++ src/sequentiell/Sequentiell.java
@@ -1,9 +1,9 @@
 package sequentiell;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
-import np2015.Neighbor; /**Das ist noch ein bisschen problematisch, aber denke bisschen googeln will do it, kann nur 
-kann nur grade nicht weil ich im zug sitze. **/
 import np2015.GraphInfo;
 import np2015.ImageConvertible;
 import np2015.GuardedCommand;
@@ -14,16 +14,31 @@ public class Sequentiell implements ImageConvertible{
 
 	public Sequentiell(GraphInfo graph){
 		this.graph = graph;
+		this.grid = graph.column2row2initialValue;
 		this.compute();
 	}
 	
+	/**Bisher wird hier nur über das Grid im ganzen iteriert, berechnet wird noch nix.**/
+	
 	private void compute(){
-		int x=0;
-		int y=0;
-		double akku;
+		HashMap<Integer, Double> column = new HashMap<>();
+		int x = 0,y = 0;/**die brauchen wir um unsere koeffizieten zu identifiezieren, dass wir getneighbor aufrufen 
+		koennen.**/
 		while(true){
-			++x;
-			graph.getRateForTarget(x, y, Left);
+			Iterator<Entry<Integer, HashMap<Integer, Double>>> columns = grid.entrySet().iterator();   
+			while(columns.hasNext()){
+				++x;
+				column = columns.next().getValue();
+				Iterator<Entry<Integer, Double>> coefficients = column.entrySet().iterator();
+				while(coefficients.hasNext()){
+					++y;
+					/*graph.column2row2initialValue(x,y,Left);*//**Wie das mit dem enum funktioniert, kA. Import oder so 
+					jedenfalls nich.**/
+					double dummy = coefficients.next().getValue(); /**hier haben wir jetzt einen grid(x,y)=greyvalue
+					 gerechnet wird aber noch nix**/
+				}
+			}
+			
 		}
 	}
 	
