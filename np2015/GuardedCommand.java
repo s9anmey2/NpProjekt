00cf9c1c8a37d5e961cbea32e287810e39051d02
Andreas <s9anmey2@stud.uni-saarland.de>
2015-08-15 14:25:58 +0200

    hmmm

--- np2015/GuardedCommand.java
+++
@@ -1,5 +0,0 @@
-package np2015;
-
-public interface GuardedCommand {
-	double getRateForTarget(final int x, final int y, Neighbor where);
-}
--- np2015/Neighbor.java
+++
@@ -1,8 +0,0 @@
-package np2015;
-
-public enum Neighbor {
-	Left,
-	Right,
-	Top,
-	Bottom
-}
